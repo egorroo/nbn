ddbbffa39e7371a2a7d46023e6f8f4fe536f0db6
e311665 <29872732-e311665@users.noreply.replit.com>
2024-02-17 12:05:17 +0000

    add some

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 mbn = []
 while True:
- fvf = int(input("Ent num:"))
- if fvf == 0:
-   print(mbn)
-   break
- else:
-     mbn.append(fvf)
+  fvf = int(input("Ent num:"))
+  if fvf == 0:
+    print(mbn)
+    break
+  else:
+    mbn.append(fvf)
 num = int(input("ent num:"))
 sym = input("Ent syb:")
 numsym = sym * num
 print(numsym)
-song = int(input("Choose song text:"))
-if song == 1:
-    print("Не придумал мне лень")
+awg = input("Wright somesing:")
+print(awg[::-1])
